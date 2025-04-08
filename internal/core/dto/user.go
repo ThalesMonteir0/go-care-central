5a8daede4b9d60187293ae9923919af819f9c759
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2025-04-08 17:12:30 -0300

    feat: adding user DTO

---
+++ internal/core/dto/user.go
@@ -0,0 +1,24 @@
+package dto
+
+import (
+	"encoding/json"
+	"go-care-central/pkg/rest_err"
+	"io"
+)
+
+type UserDTORequest struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	ClinicID int    `json:"clinic_id"`
+}
+
+func FromJsonUserDTORequest(body io.Reader) (*UserDTORequest, *rest_err.RestErr) {
+	var UserDTO UserDTORequest
+	if err := json.NewDecoder(body).Decode(&UserDTO); err != nil {
+		return nil, rest_err.NewBadRequestError("")
+	}
+
+	return &UserDTO, nil
+}
