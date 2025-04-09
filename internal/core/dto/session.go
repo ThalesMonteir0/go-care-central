d83792705c05ccb3e0f54a4e9a7bc8b4aa1810a9
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2025-04-09 09:27:29 -0300

    feat: adding session DTO

--- internal/core/dto/session.go
+++ internal/core/dto/session.go
@@ -1,1 +1,27 @@
 package dto
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"go-care-central/pkg/rest_err"
+	"io"
+	"time"
+)
+
+type SessionDTORequest struct {
+	ID           uuid.UUID `json:"id"`
+	PatientID    uuid.UUID `json:"patient_id"`
+	DateWithHour time.Time `json:"date_with_hour"`
+	Confirmed    bool      `json:"confirmed"`
+	Paid         bool      `json:"paid"`
+	Record       string    `json:"record"`
+}
+
+func FromJsonSessionDTORequest(body io.Reader) (*SessionDTORequest, *rest_err.RestErr) {
+	var SessionDTO SessionDTORequest
+	if err := json.NewDecoder(body).Decode(&SessionDTO); err != nil {
+		return nil, rest_err.NewBadRequestError("")
+	}
+
+	return &SessionDTO, nil
+}
